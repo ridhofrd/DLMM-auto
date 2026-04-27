cb7c4d1d8ac14a2d2085987e2bda928c22c28a44
macbeth <macbeth@macbeth.macbeth>
2026-04-27 20:02:30 +0700

    config.js change

--- config.js
+++ config.js
@@ -216,10 +216,10 @@ export function getDynamicTakeProfitPct() {
   const now = new Date();
   const utc7Hour = (now.getUTCHours() + 7) % 24;
 
-  if (utc7Hour >= 19 || utc7Hour < 2) return 0.75;
-  if (utc7Hour >= 2 && utc7Hour < 7) return 0.5;
-  if (utc7Hour >= 7 && utc7Hour < 13) return 0.6;
-  return 0.4;
+  if (utc7Hour >= 19 || utc7Hour < 2) return 5.0;
+  if (utc7Hour >= 2 && utc7Hour < 7) return 5.0;
+  if (utc7Hour >= 7 && utc7Hour < 13) return 5.0;
+  return 5.0;
 }
 
 export function reloadScreeningThresholds() {
